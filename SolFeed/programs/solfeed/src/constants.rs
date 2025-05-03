c7320e2ab237e248eadd41c124005f58c7bec589
SolFeed <solfeed.social@proton.me>
2025-05-03 12:20:48 +0200

    Update constants.rs
    signed

--- SolFeed/programs/solfeed/src/constants.rs
+++ SolFeed/programs/solfeed/src/constants.rs
@@ -1,7 +1,7 @@
 pub const MAX_USERNAME_LENGTH: usize = 32;
 pub const MAX_BIO_LENGTH: usize = 160;
-pub const MAX_IMAGE_SIZE: usize = 1024 * 50; // 50KB for profile or post image
-pub const MAX_COMMENT_LENGTH: usize = 280;  // Limit for comment text
+pub const MAX_IMAGE_SIZE: usize = 1024 * 50; // 50KB max size
+pub const MAX_COMMENT_LENGTH: usize = 280;
 
 // PDA Seeds
 pub const GLOBAL_SETTINGS_SEED: &[u8] = b"global-settings";
@@ -9,20 +9,6 @@ pub const USERNAME_SEED: &[u8] = b"username";
 pub const VERIFICATION_SEED: &[u8] = b"verification";
 
 
-// ╔══════════════════════════════╗  Author: @TheItalianPimp 
-// ║         ☠ THE VOID ☠         ║  Name: Luca C.
-// ╠══════════════════════════════╣  Date: 04/02/2025
-// ║                              ║  Project: SolFeed
-// ║          █████████           ║
-// ║       ███░░░░░░░░░███        ║
-// ║     ██░░░░░░░░░░░░░░░██      ║
-// ║    ██░░░░░░░░░░░░░░░░░░██    ║
-// ║   ██░░░░░░░░░░░░░░░░░░░░██   ║
-// ║   ██░░░░░░░░░░░░░░░░░░░░██   ║
-// ║   ██░░░░░░░░░░░░░░░░░░░░██   ║
-// ║    ██░░░░░░░░░░░░░░░░░░██    ║
-// ║     ██░░░░░░░░░░░░░░░██      ║
-// ║       ███░░░░░░░░░███        ║
-// ║          █████████           ║
-// ║                              ║
-// ╚══════════════════════════════╝
+//   ░░░    Author: @TheItalianPimp         lucacavallaro02@proton.me
+// ░░ + ░░  Name: Luca C.                   "the last universal common anchestor"
+//   ░░░    Project: SolFeed (Solfeed)      solfeed.social@proton.me
