b96b8c01ae94a8eefe157491d578d402c5642804
SolFeed <solfeed.social@proton.me>
2025-05-03 12:24:02 +0200

    Update global_settings.rs
    signed

--- SolFeed/programs/solfeed/src/state/global_settings.rs
+++ SolFeed/programs/solfeed/src/state/global_settings.rs
@@ -14,20 +14,6 @@ pub struct GlobalSettings {
 }
 
 
-// ╔══════════════════════════════╗  Author: @TheItalianPimp 
-// ║         ☠ THE VOID ☠         ║  Name: Luca C.
-// ╠══════════════════════════════╣  Date: 04/02/2025
-// ║                              ║  Project: SolFeed
-// ║          █████████           ║
-// ║       ███░░░░░░░░░███        ║
-// ║     ██░░░░░░░░░░░░░░░██      ║
-// ║    ██░░░░░░░░░░░░░░░░░░██    ║
-// ║   ██░░░░░░░░░░░░░░░░░░░░██   ║
-// ║   ██░░░░░░░░░░░░░░░░░░░░██   ║
-// ║   ██░░░░░░░░░░░░░░░░░░░░██   ║
-// ║    ██░░░░░░░░░░░░░░░░░░██    ║
-// ║     ██░░░░░░░░░░░░░░░██      ║
-// ║       ███░░░░░░░░░███        ║
-// ║          █████████           ║
-// ║                              ║
-// ╚══════════════════════════════╝
+//   ░░░    Author: @TheItalianPimp         lucacavallaro02@proton.me
+// ░░ + ░░  Name: Luca C.                   "the last universal common anchestor"
+//   ░░░    Project: SolFeed (Solfeed)      solfeed.social@proton.me
